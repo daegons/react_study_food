d07cd77f97aee3cecad17e19129e8949e76a87f5
daegons <119998807+daegons@users.noreply.github.com>
2023-05-02 11:58:33 +0900

    Add files via upload

--- my-app/src/components/Cart/Cart.js
+++ my-app/src/components/Cart/Cart.js
@@ -1,10 +1,30 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 const Cart = (props) => {
+  const cartCxt = useContext(CartContext);
+
+  const totalAmount = `${cartCxt.totalAmount}₩`;
+  const hasItems = cartCxt.items.length > 0;
+
+  const cartItemRemoveHandl = (id) => {};
+  const cartItemAddHandl = (item) => {};
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((list) => {
-        return <li>{list.name}</li>;
+    <ul className={classes["cart-items"]}>
+      {cartCxt.items.map((list) => {
+        return (
+          <CartItem
+            ket={list.id}
+            name={list.name}
+            amount={list.amount}
+            price={list.price}
+            cartItemRemoveHandl={cartItemRemoveHandl.bind(null, list.id)}
+            cartItemAddHandl={cartItemAddHandl.bind(null, list)}
+          />
+        );
       })}
     </ul>
   );
@@ -14,13 +34,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>최종 금액</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
           닫기
         </button>
-        <button className={classes.button}>주문</button>
+        {hasItems && <button className={classes.button}>주문</button>}
       </div>
     </Modal>
   );
--- my-app/src/store/CartProvider.js
+++ my-app/src/store/CartProvider.js
@@ -1,5 +1,5 @@
-import { useReducer } from 'react';
-import CartContext from './cart-context';
+import { useReducer } from "react";
+import CartContext from "./cart-context";
 
 const defaultCartState = {
   items: [],
@@ -7,6 +7,35 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex((it) => {
+      return it.id === action.item.id;
+    });
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
@@ -17,10 +46,13 @@ const CartProvider = (props) => {
   );
   const addItemCartHandl = (item) => {
     dispatchCartAction({
-      type: 'ADD',
+      type: "ADD",
+      item: item,
     });
   };
-  const removeItemCartHandl = (id) => {};
+  const removeItemCartHandl = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
     items: cartState.items,
